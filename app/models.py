7eb97f192b99e5e867da5164ff56e203083ac612
Christian Ternus <ternus@cternus.net>
2013-01-31 17:40:58 -0500

    ermehgerd sterhling erpderht

--- app/models.py
+++ app/models.py
@@ -94,7 +94,7 @@ class AppForm(ModelForm):
                     If you don't know what a typecast is, don't worry about it.""",
         "punts": "",
         "how_cast": "If you must be cast in a certain way, if some of your answers are more important than others, or if you have the perfect role in mind, tell us.",
-        "spy_plots": "This will help us match you with a good set of plots.",
+        "spy_plots": "This will help us match you with a good set of plots. If you've never played a spy plot before (or think you haven't), tell us that, and perhaps what spy plots you might enjoy.",
         "public_secret": """'Public authority' might be the UN Secretary General,
 the chief of police, or the Pope* in a game full of Catholics.  'Secret
 authority' might be the head of the secret police, the commander of a black-ops
