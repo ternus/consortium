06efcf2379f207682ab9d170c88a9d835ab95f2b
Christian Ternus <ternus@cternus.net>
2013-02-05 13:58:08 -0500

    migrations for territory

--- territory/migrations/0001_initial.py
+++ territory/migrations/0001_initial.py
@@ -30,6 +30,7 @@ class Migration(SchemaMigration):
         db.create_table('territory_faction', (
             ('code', self.gf('django.db.models.fields.CharField')(unique=True, max_length=256, primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('color', self.gf('django.db.models.fields.CharField')(default='', max_length=256)),
         ))
         db.send_create_signal('territory', ['Faction'])
 
@@ -46,17 +47,24 @@ class Migration(SchemaMigration):
         # Adding model 'Action'
         db.create_table('territory_action', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('turn', self.gf('django.db.models.fields.IntegerField')()),
-            ('unit', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['territory.Unit'])),
-            ('move_type', self.gf('django.db.models.fields.CharField')(max_length=256)),
-            ('move_to', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='t_move_to', null=True, to=orm['territory.Territory'])),
+            ('faction', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['territory.Faction'])),
+            ('turn', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True)),
+            ('territory', self.gf('django.db.models.fields.related.ForeignKey')(related_name='t_territory', to=orm['territory.Territory'])),
+            ('type', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('target', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='t_move_to', null=True, to=orm['territory.Territory'])),
             ('special', self.gf('django.db.models.fields.CharField')(max_length=256, blank=True)),
             ('support_type', self.gf('django.db.models.fields.CharField')(max_length=256, blank=True)),
-            ('support_from', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='t_supp_from', null=True, to=orm['territory.Territory'])),
             ('support_to', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='t_supp_to', null=True, to=orm['territory.Territory'])),
-            ('succeeded', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('validation_level', self.gf('django.db.models.fields.IntegerField')(default=0)),
         ))
         db.send_create_signal('territory', ['Action'])
+
+        # Adding model 'GameBoard'
+        db.create_table('territory_gameboard', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('turn', self.gf('django.db.models.fields.IntegerField')(default=0)),
+        ))
+        db.send_create_signal('territory', ['GameBoard'])
 
 
     def backwards(self, orm):
@@ -75,31 +83,40 @@ class Migration(SchemaMigration):
         # Deleting model 'Action'
         db.delete_table('territory_action')
 
+        # Deleting model 'GameBoard'
+        db.delete_table('territory_gameboard')
+
 
     models = {
         'territory.action': {
             'Meta': {'object_name': 'Action'},
+            'faction': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['territory.Faction']"}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'move_to': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'t_move_to'", 'null': 'True', 'to': "orm['territory.Territory']"}),
-            'move_type': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
             'special': ('django.db.models.fields.CharField', [], {'max_length': '256', 'blank': 'True'}),
-            'succeeded': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'support_from': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'t_supp_from'", 'null': 'True', 'to': "orm['territory.Territory']"}),
             'support_to': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'t_supp_to'", 'null': 'True', 'to': "orm['territory.Territory']"}),
             'support_type': ('django.db.models.fields.CharField', [], {'max_length': '256', 'blank': 'True'}),
-            'turn': ('django.db.models.fields.IntegerField', [], {}),
-            'unit': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['territory.Unit']"})
+            'target': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'t_move_to'", 'null': 'True', 'to': "orm['territory.Territory']"}),
+            'territory': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'t_territory'", 'to': "orm['territory.Territory']"}),
+            'turn': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'validation_level': ('django.db.models.fields.IntegerField', [], {'default': '0'})
         },
         'territory.faction': {
             'Meta': {'object_name': 'Faction'},
             'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '256', 'primary_key': 'True'}),
+            'color': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '256'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        'territory.gameboard': {
+            'Meta': {'object_name': 'GameBoard'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'turn': ('django.db.models.fields.IntegerField', [], {'default': '0'})
         },
         'territory.territory': {
             'Meta': {'object_name': 'Territory'},
             'center_s': ('django.db.models.fields.CharField', [], {'max_length': '256', 'blank': 'True'}),
             'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '256', 'primary_key': 'True'}),
-            'connects': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'t_connects'", 'symmetrical': 'False', 'to': "orm['territory.Territory']"}),
+            'connects': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'connects_rel_+'", 'to': "orm['territory.Territory']"}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
             'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['territory.Faction']", 'null': 'True', 'blank': 'True'}),
             'pts_s': ('django.db.models.fields.CharField', [], {'max_length': '512', 'blank': 'True'})
