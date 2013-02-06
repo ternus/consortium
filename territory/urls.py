55684821f2ed5126862cc7fc4f68ee4ea5ebb41a
Christian Ternus <ternus@cternus.net>
2013-02-05 22:38:37 -0500

    Remove faction ID from territory control

--- territory/urls.py
+++ territory/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^(?P<faction_code>\w+)/$', 'territory.views.overview', name='overview'),
+    url(r'^$', 'territory.views.overview', name='overview'),
+
+#    url(r'^(?P<faction_code>\w+)/$', 'territory.views.overview', name='overview'),
 #    url(r'^line/(\d+)/$', 'succession.views.show_line', name='show_line'),
 
 )
--- territory/views.py
+++ territory/views.py
@@ -5,14 +5,14 @@ from territory.models import Unit, Faction, Territory
 from territory.somalia import somaliax as somalia_pts
 
 @login_required()
-def overview(request, faction_code, template="territory/territory.html"):
+def overview(request, template="territory/territory.html"):
     somalia = ",".join(["[%s,%s]" %(x[0],x[1]) for x in Territory.convert_points(somalia_pts)])
-    faction = get_object_or_404(Faction, code=faction_code)
-    units = Unit.live_units().filter(faction=faction)
+#    faction = get_object_or_404(Faction, code=faction_code)
+#    units = Unit.live_units().filter(faction=faction)
     territories = Territory.objects.all() #TODO can_see?
     context = {
         'somalia': somalia,
-        'units':units,
+#        'units':units,
         'territories': territories,
     }
     return render(request, template, context)
