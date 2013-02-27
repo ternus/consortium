f6897c85dd08ba824ef6acea7e38c89281e55d1e
Christian Ternus <ternus@cternus.net>
2013-02-26 22:35:18 -0500

    Remind everyone link

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     url(r'^$', 'app.views.app', name='app'),
     url(r'^dashboard/$', 'app.views.dashboard', name='dashboard'),
     url(r'^remind/(?P<app_id>\w+)/$', 'app.views.remind', name='remind'),
-    url(r'^remind_everyone/$', 'app.views.remind', name='remind'),
+    url(r'^remind_everyone/$', 'app.views.remind_everyone', name='remind_everyone'),
 
     url(r'^(?P<app_id>\w+)/$', 'app.views.app', name='app'),
 )
