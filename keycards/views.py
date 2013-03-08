126ab7f72105786f45aa5a948ba3f37dfe7882c1
Christian Ternus <ternus@cternus.net>
2013-03-08 02:26:22 -0500

    keycards

--- keycards/views.py
+++ keycards/views.py
@@ -6,10 +6,8 @@ def keycard(request, template="keycards/show_keycard.html"):
     number = request.GET.get('number', None)
     found = False
     kc = None
-    print number
     kcs = GTO.bc('KeyCard').filter(gametexfieldvalue__field__name='number',
                                        gametexfieldvalue__value=number)
-    print kcs
     if kcs.exists():
         kc = kcs[0]
     context = {'found':found,
