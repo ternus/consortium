1cb1ede1b22b5642b00c98a3caeca342c09b30af
Christian Ternus <ternus@cternus.net>
2013-03-09 19:47:17 -0500

    safety on stuff

--- hexgrid/tasks.py
+++ hexgrid/tasks.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from django.utils.timezone import now
 from consortium.consortium import send_mail
 from hexgrid.models import GameDay
 from celery.schedules import crontab
@@ -8,6 +9,7 @@ __author__ = 'cternus'
 
 @periodic_task(run_every=crontab(minute='0', hour='6'))
 def market_tick():
+    if now().time().hour < 5 or now().time().hour > 6: return "Wrongly fired"
     msg = GameDay.tick()
     send_mail("[Consortium] Tick!", "Tick successful at %s" % datetime.now(), 'consortium-gms@cternus.net',
               ['ternus@mit.edu'], fail_silently=True)
--- territory/tasks.py
+++ territory/tasks.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from django.utils.timezone import now
 from consortium.consortium import send_mail
 from hexgrid.models import GameDay
 from celery.schedules import crontab
@@ -8,16 +9,17 @@ from territory.models import GameBoard
 __author__ = 'cternus'
 
 @periodic_task(run_every=crontab(minute='0', hour='19'))
-def day_phase_foo():
+def day_phase():
+    if now().time().hour < 18 or now().time().hour > 19: return "Wrongly fired"
     g = GameBoard.objects.get()
     s = g.execute_turn(string=True)
     send_mail("[Consortium] Day phase complete", "Day phase successful at %s\n%s" % (datetime.now(), s), 'consortium-gms@cternus.net',
           ['ternus@cternus.net'], fail_silently=True)
 
 @periodic_task(run_every=crontab(minute='0', hour='23'))
-def night_phase_foo():
+def night_phase():
+    if now().time().hour < 22: return "Wrongly fired"
     g = GameBoard.objects.get()
     s = g.execute_turn(string=True)
     send_mail("[Consortium] Night phase complete", "Night phase successful at %s\n%s" % (datetime.now(), s), 'consortium-gms@cternus.net',
           ['ternus@cternus.net'], fail_silently=True)
-
