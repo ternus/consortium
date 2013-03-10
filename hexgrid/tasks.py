980a66d7cdd20d64462729632d64ccbbeba065e1
Christian Ternus <ternus@cternus.net>
2013-03-10 08:09:35 -0400

    fix

--- hexgrid/tasks.py
+++ hexgrid/tasks.py
@@ -9,7 +9,7 @@ __author__ = 'cternus'
 
 @periodic_task(run_every=crontab(minute='0', hour='6'))
 def market_tick():
-    if now().time().hour < 5 or now().time().hour > 6: return "Wrongly fired"
+    # if now().time().hour < 5 or now().time().hour > 6: return "Wrongly fired"
     msg = GameDay.tick()
     send_mail("[Consortium] Tick!", "Tick successful at %s" % datetime.now(), 'consortium-gms@cternus.net',
               ['ternus@mit.edu'], fail_silently=True)
