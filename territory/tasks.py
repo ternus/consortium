d95180cc053422c5a5ef3c7675e8c72fe438c094
Christian Ternus <ternus@cternus.net>
2013-03-10 19:18:34 -0400

    test

--- territory/tasks.py
+++ territory/tasks.py
@@ -8,17 +8,19 @@ from territory.models import GameBoard
 
 __author__ = 'cternus'
 
-@periodic_task(run_every=crontab(minute='0', hour='19'))
+def test_task():
+    send_mail("[Consortium] Test!", "Day phase successful at %s\n%s" % (datetime.now(), s), 'consortium-gms@cternus.net',
+          ['ternus@cternus.net'], fail_silently=True)
+
+# @periodic_task(run_every=crontab(minute='0', hour='19'))
 def day_phase():
-    # if now().time().hour < 18 or now().time().hour > 19: return "Wrongly fired"
     g = GameBoard.objects.get()
     s = g.execute_turn(string=True)
     send_mail("[Consortium] Day phase complete", "Day phase successful at %s\n%s" % (datetime.now(), s), 'consortium-gms@cternus.net',
           ['ternus@cternus.net'], fail_silently=True)
 
-@periodic_task(run_every=crontab(minute='0', hour='23'))
+# @periodic_task(run_every=crontab(minute='0', hour='23'))
 def night_phase():
-    # if now().time().hour < 22: return "Wrongly fired"
     g = GameBoard.objects.get()
     s = g.execute_turn(string=True)
     send_mail("[Consortium] Night phase complete", "Night phase successful at %s\n%s" % (datetime.now(), s), 'consortium-gms@cternus.net',
