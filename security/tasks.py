afb601b596b01bae896acd09cd6d6c4a5ed5937a
Christian Ternus <ternus@cternus.net>
2013-03-10 20:01:19 -0400

    mail alert

--- security/tasks.py
+++ security/tasks.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 from celery.schedules import crontab
 from celery.task import periodic_task
 from django.utils.timezone import now
+from consortium.consortium import send_mail
 from security.models import EntryWindow
 
 __author__ = 'cternus'
@@ -11,3 +12,5 @@ def check_security_windows():
     windows = EntryWindow.objects.filter(start_time__lte=now(), start_time__gt=now() - timedelta(hours=1))
     for w in windows:
         w.check_and_notify()
+        send_mail("[Consortium] Security window alert!", "Security window triggered: %s" % w, 'consortium-gms@cternus.net',
+                  ['ternus@mit.edu'], fail_silently=True)
